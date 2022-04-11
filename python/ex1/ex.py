8c10b4c5adda2cb6760ae240aad416b732c936c0
ThomGosselin <thomswim360@gmail.com>
2022-04-10 21:20:33 -0400

    Lean addition // end of EX1

--- python/ex1/ex.py
+++ python/ex1/ex.py
@@ -1,5 +1,11 @@
 number1 = 20
 number2 = 30
 
-finalnumber = number1 * number2;
-print(finalnumber);
+number3 = 40
+number4 = 30
+
+finalnumber1 = number1 * number2;
+finalnumber2 = number3 + number4;
+
+print(finalnumber1);
+print(finalnumber2);
